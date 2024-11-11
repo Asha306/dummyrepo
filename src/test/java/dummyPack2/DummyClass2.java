6e807ddc4cdde206f007e129ca7d3d6e15beb4b3
Asha306 <reddyasha.21@gmail.com>
2024-11-11 14:36:27 +0530

    commit done

---
+++ src/test/java/dummyPack2/DummyClass2.java
@@ -0,0 +1,8 @@
+package dummyPack2;
+
+public class DummyClass2 {
+	public static void main(String[] args) {
+		System.out.println("bye");
+	}
+
+}
---
+++ src/test/java/dummyPackage/dummyClass.java
@@ -0,0 +1,7 @@
+package dummyPackage;
+
+public class dummyClass {
+public static void main(String[] args) {
+	System.out.println("hiii");
+}
+}
